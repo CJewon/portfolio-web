b333318a2401a2f1ae94f70163374b3707e1cf4e
CJewon <112820744+CJewon@users.noreply.github.com>
2024-01-15 13:56:29 +0900

    start

---
+++ profile-site/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header>
+          <a href="#idPos">MAIN</a>
+          <a href="#">INTRODUCE</a>
+          <a href="#">WORK</a>
+          <a href="#">CONTACT</a>
+        </header>
+        <div className="main-container" id='idPos'>
+          
+        </div>
+    </div>
+  );
+}
+
+export default App;
