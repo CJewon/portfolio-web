e10e5f955313b2ecae65fe3e75363fb3f0df007e
CJewon <112820744+CJewon@users.noreply.github.com>
2024-01-18 14:51:00 +0900

    aaaa

--- profile-site/src/App.js
+++ profile-site/src/App.js
@@ -15,14 +15,16 @@ function App() {
         <section className="main-section" id='idPos'>
           <div className="main-container">
             <div className="title-container">
-              <h1>WELCOME TO MY<br/>PORTFOLIO</h1>
+              <h1>
+                <span>WELCOME TO MY</span>
+                <span>PORTFOLIO</span></h1>
               <p>안녕하세요! 끊임없이 도전하고, 나아가 프론트엔드 개발자가 되고 싶은 신입 웹 퍼블리셔 최제원입니다.</p>
             </div>
+          </div>
             <div className="copyright-container">
               <p>본 페이지는 상업적 목적이 아닌 개인 포트폴리오용으로 만들어진 사이트입니다.</p>
               <p>CHOIJEWONⓒ 2023 PORTFOLIO</p>
             </div>
-          </div>
         </section>
         <section className='introduce-section' id='introducePos'>
           <h2>INTRODUCE</h2>
