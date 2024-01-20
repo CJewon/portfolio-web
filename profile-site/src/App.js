262591bf2197bf6886754921d87fa9b73d2102cc
CJewon <112820744+CJewon@users.noreply.github.com>
2024-01-20 20:07:17 +0900

    css with img

--- profile-site/src/App.js
+++ profile-site/src/App.js
@@ -17,7 +17,8 @@ function App() {
             <div className="title-container">
               <h1>
                 <span>WELCOME TO MY</span>
-                <span>PORTFOLIO</span></h1>
+                <span>PORTFOLIO</span>
+              </h1>
               <p>안녕하세요! 끊임없이 도전하고, 나아가 프론트엔드 개발자가 되고 싶은 신입 웹 퍼블리셔 최제원입니다.</p>
             </div>
           </div>
@@ -28,18 +29,21 @@ function App() {
         </section>
         <section className='introduce-section' id='introducePos'>
           <h2>INTRODUCE</h2>
-          <div className="img-container">
-            <img src="#" alt="#" />
-          </div>
-          <div className="introduce-text-container">
-            <h3>안녕하세요! 신입 웹 퍼블리셔 최제원입니다.</h3>
-            <p>웹 개발에 관심이 많아 열심히 공부중인 신입 웹퍼블리셔입니다.
-                새로운 것을 만들고 구현할때마다 벅참과 성취감에 취해 즐겁게 공부하고 있습니다.  HTML, CSS, Javascript가 가능하며, 
-                현재는 Javascript 라이브러리인 React를 공부하여 다양한
-                프레임워크 환경에서 작업해 나아가는것을 목표로 기초를 다지고 있습니다.
-                저의 개발자로써의 목표는 많은 경험과 지식, 그리고 소통으로
-                끊임없이 발전해 나아가는 프론트엔드 개발자가 되는것입니다. 
-            </p>
+          <div className="introduce-container">
+            <div className="img-container">
+              <img src="/img/myImg.jpg" alt="내이미지" />
+            </div>
+            <div className="introduce-text-container">
+              <h3>안녕하세요! 신입 웹 퍼블리셔 <span>최제원</span>입니다.</h3>
+              <p>웹 개발에 관심이 많아 열심히 공부중인 신입 웹퍼블리셔입니다.</p>
+              <p>새로운 것을 만들고 구현할때마다 벅참과 성취감에 취해 즐겁게 공부하고 있습니다.  HTML,<br></br> CSS, Javascript가 가능하며, 
+                  현재는 Javascript 라이브러리인 React를 공부하여 다양한<br></br>
+                  프레임워크 환경에서 작업해 나아가는것을 목표로 기초를 다지고 있습니다.
+              </p>
+              <p>저의 개발자로써의 목표는 많은 경험과 지식, 그리고 <span>소통</span>으로
+                  끊임없이 발전해 나아가는<br></br> <span>프론트엔드 개발자</span>가 되는것입니다. 
+              </p>
+            </div>
           </div>
         </section>
         <section className='work-section' id='workPos'>
