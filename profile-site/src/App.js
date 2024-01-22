8ee4bddb1d669b38b3169dec9eabb9883b9eaf83
CJewon <112820744+CJewon@users.noreply.github.com>
2024-01-22 15:18:53 +0900

    introduce css clear

--- profile-site/src/App.js
+++ profile-site/src/App.js
@@ -1,5 +1,5 @@
+import './reset.css';
 import './App.css';
-
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
                 <span>WELCOME TO MY</span>
                 <span>PORTFOLIO</span>
               </h1>
-              <p>안녕하세요! 끊임없이 도전하고, 나아가 프론트엔드 개발자가 되고 싶은 신입 웹 퍼블리셔 최제원입니다.</p>
+              <p>안녕하세요!끊임없이 도전하고, 나아가 프론트엔드 개발자가 되고 싶은 신입 웹 퍼블리셔 최제원입니다.</p>
             </div>
           </div>
           <div className="copyright-container">
@@ -34,15 +34,72 @@ function App() {
               <img src="/img/myImg.jpg" alt="내이미지" />
             </div>
             <div className="introduce-text-container">
-              <h3>안녕하세요! 신입 웹 퍼블리셔 <span>최제원</span>입니다.</h3>
-              <p>웹 개발에 관심이 많아 열심히 공부중인 신입 웹퍼블리셔입니다.</p>
-              <p>새로운 것을 만들고 구현할때마다 벅참과 성취감에 취해 즐겁게 공부하고 있습니다.  HTML,<br></br> CSS, Javascript가 가능하며, 
-                  현재는 Javascript 라이브러리인 React를 공부하여 다양한<br></br>
-                  프레임워크 환경에서 작업해 나아가는것을 목표로 기초를 다지고 있습니다.
-              </p>
-              <p>저의 개발자로써의 목표는 많은 경험과 지식, 그리고 <span>소통</span>으로
-                  끊임없이 발전해 나아가는<br></br> <span>프론트엔드 개발자</span>가 되는것입니다. 
-              </p>
+              <h3>안녕하세요!<br></br>신입 웹 퍼블리셔 <span>최제원</span>입니다.</h3>
+              <div className="introduce-text">
+                <p>웹 개발에 관심이 많아 열심히 공부중인 신입 웹퍼블리셔입니다.</p>
+                <p>새로운 것을 만들고 구현할때마다 벅참과 성취감에 취해 즐겁게 공부하고 있습니다.  HTML,<br></br> CSS, Javascript가 가능하며, 
+                    현재는 Javascript 라이브러리인 React를 공부하여 다양한<br></br>
+                    프레임워크 환경에서 작업해 나아가는것을 목표로 기초를 다지고 있습니다.
+                </p>
+                <p>저의 개발자로써의 목표는 많은 경험과 지식, 그리고 <span>소통</span>으로
+                    끊임없이 발전해 나아가는<br></br> <span>프론트엔드 개발자</span>가 되는것입니다. 
+                </p>
+              </div>
+            <div className="stack-graph-container">
+              <h3><span>STACK</span></h3>
+              <div className="stack-graph-circle-container">
+                <ul className="circle-ul">
+                  <li className="circle-li">
+                    <div className="circle-img">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle opacity="0.2" cx="50" cy="50" r="46" stroke="#D9D9D9" stroke-width="8"/>
+                      </svg>
+                      <svg className='percent-bar' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle cx="50" cy="50" r="46" stroke="#0ACAD7" stroke-width="8"/>
+                      </svg>
+                      <img src="/img/html.png" alt="html" className='icon-img' />
+                    </div>
+                    <p className='percent'>50%</p>
+                  </li>
+                  <li className="circle-li">
+                    <div className="circle-img">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle opacity="0.2" cx="50" cy="50" r="46" stroke="#D9D9D9" stroke-width="8"/>
+                      </svg>
+                      <svg className='percent-bar' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle cx="50" cy="50" r="46" stroke="#0ACAD7" stroke-width="8"/>
+                      </svg>
+                      <img src="/img/css.png" alt="css" className='icon-img' />
+                    </div>
+                    <p className='percent'>50%</p>
+                  </li>
+                  <li className="circle-li">
+                    <div className="circle-img">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle opacity="0.2" cx="50" cy="50" r="46" stroke="#D9D9D9" stroke-width="8"/>
+                      </svg>
+                      <svg className='percent-bar' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle cx="50" cy="50" r="46" stroke="#0ACAD7" stroke-width="8"/>
+                      </svg>
+                      <img src="/img/js.png" alt="js" className='icon-img' />
+                    </div>
+                    <p className='percent'>50%</p>  
+                  </li>
+                  <li className="circle-li">
+                    <div className="circle-img">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle opacity="0.2" cx="50" cy="50" r="46" stroke="#D9D9D9" stroke-width="8"/>
+                      </svg>
+                      <svg className='percent-bar' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                      <circle cx="50" cy="50" r="46" stroke="#0ACAD7" stroke-width="8"/>
+                      </svg>
+                      <img src="/img/react.png" alt="react" className='icon-img' />
+                    </div>
+                    <p className='percent'>50%</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
             </div>
           </div>
         </section>
